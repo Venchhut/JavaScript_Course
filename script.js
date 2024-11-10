d3734ce8f866db0d217561cb830281ef4e0e5105
Venchhut <ijinbaekk@gmail.com>
2024-11-10 23:07:18 +0700

    callback function

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 // 1
-// Array start with index 0
-const singer = ["vanda", "G-devit", "Tena", "Songha"];
-console.log(singer[0]);
-console.log(singer[1]);
-console.log(singer[2]);
-console.log(singer[3]);
-// singer.push("Khmer muy jivit"); add new to array
-// singer.pop();
-// singer.unshift("Rego");
-let NumOfsinger = singer.length;
-let index = singer.indexOf("vanda");
-console.log(NumOfsinger);
-console.log(index);
 
-// 2
-const singers = ["vanda", "G-devit", "Tena", "Songha"];
-
-// for (i = 0; i <= singers.length; i++) {
-//   console.log(singers[i]);
+// function hello() {
+//   setTimeout(function () {
+//     console.log("hello");
+//   }, 2000);
 // }
 
-// 3
-singers.sort().reverse();
-for (let singer of singers) {
-  console.log(singer);
+// function welcome() {
+//   console.log("welcome");
+// }
+
+// hello();
+// welcome();
+
+// 2
+function weather(callback) {
+  console.log("hot");
+  callback();
 }
+function season() {
+  console.log("rainy");
+}
+function wait() {
+  console.log("wait");
+}
+
+weather(wait);
+
+// 3
+function sum(callback, x, y) {
+  let sumNum = x * y;
+  callback(sumNum);
+}
+function DisplaySum(sumNum) {
+  console.log(sumNum);
+}
+sum(DisplaySum, 78, 20);
+
+// 4
+
+function call(callback) {
+  let song = "The day we met";
+
+  console.log(`Do you remember ${song}?`);
+  callback();
+}
+function answer() {
+  console.log("let's get it back");
+}
+
+call(answer);
