c7607852e35101e7ffa8117d750334583b9c15b6
Venchhut <ijinbaekk@gmail.com>
2024-11-10 21:04:10 +0700

    function

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-// for loop
+// function
+// 1
+function hello() {
+  console.log("this is function");
+}
 
-for (i = 1; i <= 10; i++) {
-  if (i == 9) {
-    continue;
+hello();
+
+// 2
+function welcome(name, age) {
+  console.log(`nice to meet you ${name}`);
+  console.log(`i'm ${age} years old`);
+}
+
+welcome("seyha", 18);
+// 3
+
+function sum(x, y) {
+  let result = x + y;
+  return result;
+}
+let number = sum(2, 4);
+console.log(number);
+// console.log(sum(2, 4));
+
+// 4
+
+function isEven(number) {
+  if (number % 2 === 0) {
+    return true;
   } else {
-    console.log(i + "." + "i love you ");
+    return false;
   }
 }
+console.log(isEven(10));
+
+// or write ternary operation
+function isEven(number) {
+  return number % 2 === 0 ? true : false;
+}
+console.log(isEven(3));
+
+// 5
+function isValidEmail(email) {
+  if (email.includes("@")) {
+    return "Your email is valid";
+  } else {
+    return "You need to check again";
+  }
+}
+console.log(isValidEmail("example@gmail.com"));
+console.log(isValidEmail("examplegmail.com"));
