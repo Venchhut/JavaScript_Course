28002786f1abc18626aa41febd41199e2e7ff4aa
Venchhut <ijinbaekk@gmail.com>
2024-11-16 18:09:50 +0700

    first

--- script.js
+++ script.js
@@ -1,41 +1,45 @@
 // 1
-//simple object
-const user = {
-  name: "John",
-  age: 30,
-};
 
-const admin = {
-  name: "Jane",
-  age: 28,
-};
-
-const moderator = {
-  name: "Bob",
-  age: 35,
-};
-
-console.log(user.age);
-console.log(admin.age);
-console.log(moderator.name);
-
-// 2
-// use Contructor
-function User(name, age) {
-  this.name = name;
-  this.age = age;
-  this.isAdmin = () => {
-    if (this.name === "John") {
-      return true;
-    }
-    return false;
-  };
+class Rectagle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 }
 
-const user1 = new User("John", 30);
-const user2 = new User("Jane", 28);
-const user3 = new User("Bob", 35);
-console.log(user1.age);
-console.log(user2.age);
-console.log(user3.name);
-console.log(user1.isAdmin());
+const square = new Rectagle(10, 30);
+console.log(square);
+
+//2
+
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  applyDiscount(discount) {
+    const newPrice = this.price * (1 - discount);
+    this.price = newPrice;
+  }
+}
+
+const book = new Product("Book", 20);
+book.applyDiscount(0.5);
+console.log(book);
+
+// 3
+
+class Products {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  discount() {
+    const newPrice = this.price * 0.6;
+    this.price = newPrice;
+  }
+}
+const product1 = new Products("Iphone", "999");
+product1.discount();
+console.log(product1.price);
