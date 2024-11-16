e3daf491af72501fd6d8c10e076a238109d56647
Venchhut <ijinbaekk@gmail.com>
2024-11-16 22:55:10 +0700

    first commit

--- script.js
+++ script.js
@@ -1,55 +1,71 @@
-//1
-// use to swap values
-let a = 1;
-let b = 2;
+// 1
 
-[a, b] = [b, a];
+// const person = {
+//   fullName: "Naruto Uzumaki",
+//   position: "Jujutsu Kaisen",
+//   age: 19,
+//   job: "Ninja",
+//   hobbies: ["reading manga", "watching anime", "playing video games"],
+//   address: {
+//     city: "Konoha",
+//     state: "Hidden Leaf Village",
+//     country: "Naruto Land",
+//     postalCode: 12345,
+//   },
+// };
 
-console.log(a);
-console.log(b);
+// console.log(person.fullName);
+// console.log(person.hobbies[0]);
+// console.log(person.hobbies[2]);
+// console.log(person.address.city);
 
-//2
-//  swap 2 element in array
+// 2
 
-const foods = ["apple", "banana", "orange", "grape", "pineapple"];
-
-[foods[0], foods[2]] = [foods[2], foods[0]];
-
-console.log(foods);
-
-// 3
-// assign multiple values to multiple variables
-
-[first, second, third, ...rest] = foods;
-
-console.log(first);
-console.log(second);
-console.log(third);
-console.log(rest);
-
-// 4
-// object destructuring
-
-const person = {
-  name: "Tena Wong",
-  age: "20",
-  address: "Kathmandu",
-};
-
-const { name, age, address, job = "unemployed" } = person;
-
-console.log(`name:${name}`);
-console.log(age);
-console.log(address);
-console.log(job);
-
-// 5
-// destructuring in functions parameters
-
-function DisplayPerson({ name, age, address }) {
-  console.log(`name:${name}`);
-  console.log(`age:${age}`);
-  console.log(address);
+class Person {
+  constructor(fullName, position, age, job, hobbies, ...address) {
+    this.fullName = fullName;
+    this.position = position;
+    this.age = age;
+    this.job = job;
+    this.hobbies = hobbies;
+    this.address = new Address(...address);
+  }
 }
 
-DisplayPerson(person);
+class Address {
+  constructor(street, city, state) {
+    this.street = street;
+    this.city = city;
+    this.state = state;
+  }
+}
+
+const person = new Person(
+  "Naruto Uzumaki",
+  "Jujutsu Kaisen",
+  19,
+  "Ninja",
+  ["reading manga", "watching anime", "playing video games"],
+  "Konoha",
+  "Hidden Leaf Village",
+  "Naruto Land",
+  12345
+);
+console.log(person.hobbies);
+console.log(person.address.state);
+
+// 3
+
+let Persons = {
+  name: "Victor",
+  contact: {
+    phone: "123-456-7890",
+    email: "Rakie@example.com",
+    address: {
+      street: "123 maere street",
+      city: "phnom penh",
+    },
+  },
+};
+console.log(Persons.contact.email);
+console.log(Persons.contact.address.city);
