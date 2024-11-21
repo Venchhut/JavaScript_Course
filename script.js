f76aa7f156a4aa29d28ce680f176630b91c1beb9
Venchhut <ijinbaekk@gmail.com>
2024-11-21 09:03:15 +0700

    second

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-// console.log(document);
+// 1. document.getElementById() : Element or null
+//2. document.getElementsByClassName() : HTMLCollection
+//3. document.getElementsByTagName() : HTMLCollection
+//4. document.querySelector() : Element or null
+//5. document.querySelectorAll() : nodelist
 
-// console.dir(document);
+//1
+const TitleStyle = document.getElementById("title");
+TitleStyle.style.backgroundColor = "yellow";
+TitleStyle.style.textAlign = "center";
+TitleStyle.style.fontSize = "30px";
 
-document.body.style.backgroundColor = "grey";
+TitleStyle.style.alignItems = "center";
+//2
+const listItems = document.querySelectorAll("li");
+listItems[1].style.color = "green";
+listItems[2].style.color = "red";
 
-const title = "";
+listItems.forEach((item) => {
+  item.style.fontSize = "20px";
+});
+console.log(listItems);
 
-const titleDom = document.getElementById("sub-title");
-
-titleDom.textContent += title === "" ? "Nothing" : title;
-
-const demoDom = document.getElementById("demo");
-
-demoDom.textContent = "Hello";
+//3
