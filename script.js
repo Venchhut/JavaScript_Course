fd6dfaf0390ad1709847cc6a4cc1966f49d15369
Venchhut <ijinbaekk@gmail.com>
2024-11-21 20:54:24 +0700

    add

--- script.js
+++ script.js
@@ -22,3 +22,51 @@ listItems.forEach((item) => {
 console.log(listItems);
 
 //3
+// get elementsbyclassname
+// html collection of elements with the same class name is returned.
+
+const happy = document.getElementsByClassName("fun");
+happy[1].style.fontSize = "20px";
+for (let item of happy) {
+  item.style.backgroundColor = "blue";
+}
+// we can not use foreach directly on html collection because it is not an array. so i make it into an array that we can use foreach
+Array.from(happy).forEach((item) => (item.style.textAlign = "center"));
+
+console.log(happy);
+
+// 4
+// get element by tagName
+//  is used to select elements by their tag name.
+
+const h3Element = document.getElementsByTagName("h3");
+h3Element[0].style.backgroundColor = "grey";
+h3Element[0].style.textAlign = "center";
+console.log(h3Element);
+
+const LiElement = document.getElementsByTagName("li");
+
+Array.from(listItems).forEach((item) => {
+  item.style.textAlign = "center";
+  item.style.color = "red";
+});
+
+// 5
+// querySelector
+// querySelector() is used to select a single element based on a CSS selector.
+// First element or null
+const demoElement = document.querySelector("#demo");
+demoElement.style.textAlign = "center";
+demoElement.style.fontSize = "20px";
+demoElement.style.color = "green";
+
+// querySelectorAll
+// querySelectorAll() is used to select all elements that match a CSS selector.
+// notelist
+
+const ulElement = document.querySelectorAll("ul");
+// ulElement[0].style.backgroundColor = "grey";
+// ulElement[0].style.textAlign = "center";
+console.log(ulElement);
+
+ulElement.forEach((item) => (item.style.backgroundColor = "grey"));
