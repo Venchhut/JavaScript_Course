41ad695a9a7a5fa856e2d0aa76b37aa269be9d46
Venchhut <ijinbaekk@gmail.com>
2024-12-16 14:30:58 +0700

    add more method

--- script.js
+++ script.js
@@ -16,32 +16,60 @@ const data = [
     address: { id: 4, place: "sihanoukville" },
   },
 ];
-
-// const filteredData = data.reduce((result, current) => {
-//   // Check if the address place already exists in the result array
-//   const exists = result.find(
-//     (item) => item.address.place === current.address.place
-//   );
-
-//   if (!exists) {
-//     // If not exists, add to the result array
-//     result.push(current);
-//   } else {
-//     // If exists, append the name to the existing object
-//     exists.name = Array.isArray(exists.name)
-//       ? [...exists.name, current.name]
-//       : [exists.name, current.name];
-//   }
-
-//   return result;
-// }, []);
-
-// console.log(filteredData);
-
+// using reduce
 const filteredData = data.reduce((result, current) => {
+  // Check if the address place already exists in the result array
+  const exists = result.find(
+    (item) => item.address.place === current.address.place
+  );
+
+  if (!exists) {
+    // If not exists, add to the result array
+    result.push(current);
+  } else {
+    // If exists, append the name to the existing object
+    exists.name = Array.isArray(exists.name)
+      ? [...exists.name, current.name]
+      : [exists.name, current.name];
+  }
+
+  return result;
+}, []);
+
+console.log(filteredData);
+
+// using Set in js
+
+const filteredDataSet = data.reduce((result, current) => {
+  const addressSet = new Set(result.map((item) => item.address.place));
+  if (!addressSet.has(current.address.place)) {
+    addressSet.add(current.address.place);
+    result.push(current);
+  } else {
+    const existingAddress = result.find(
+      (item) => item.address.place === current.address.place
+    );
+    existingAddress.name = Array.isArray(existingAddress.name)
+      ? [...existingAddress.name, current.name]
+      : [existingAddress.name, current.name];
+  }
+  return result;
+}, []);
+console.log(filteredDataSet);
+// using reduce
+const filteredData2 = data.reduce((result, current) => {
+  JSON.stringify(result);
   return result.includes(current.address.place)
     ? result
     : [current.address.place, ...result];
 }, []);
+console.log(filteredData2);
 
-console.log(filteredData);
+const filteredData3 = data.reduce((result, current) => {
+  const addressSet = new Set(result);
+  if (!addressSet.has(current.address.place)) {
+    addressSet.add(current.address.place);
+  }
+  return [...addressSet];
+}, []);
+console.log(filteredData3);
