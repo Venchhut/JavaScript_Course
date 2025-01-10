803860b800d8c4c281426b47d8b2964871c3a076
Venchhut <ijinbaekk@gmail.com>
2025-01-10 13:15:09 +0700

    add

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// type of the async operation
+// Callback
+// Promise
+// Async/Await
+// Event listener...
+
 // 1
 //async function
 setTimeout(() => {
@@ -7,6 +13,13 @@ console.log("Task 2");
 console.log("Task 3");
 console.log("Task 4");
 
+//
+console.log("first");
+setTimeout(() => {
+  console.log("this will be executed after 1 seconds");
+}, 1000);
+console.log("end the program");
+
 // 2
 function task(callback) {
   setTimeout(() => {
@@ -14,17 +27,25 @@ function task(callback) {
     callback();
   }, 2000);
 }
-
 function follow() {
   console.log("Task 2:: drink water");
   console.log("Task 3: eat breakfast");
   console.log("Task 4: go to work");
 }
-
 task(follow);
 
-// 3
+function job(callback) {
+  setTimeout(() => {
+    console.log("program is running");
+    callback();
+  }, 2000);
+}
+function enrivement() {
+  console.log("peace of mind");
+}
+job(enrivement);
 
+//3  using promise
 function asyncFunction() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -33,15 +54,35 @@ function asyncFunction() {
     }, 2000);
   });
 }
-
 asyncFunction().then(() => {
   console.log("Task 2");
   console.log("Task 3");
-  console.log("Task 4");
+  console.log("Task 4 completed");
 });
 
-// 4
+function workTime() {
+  return new Promise((resolve, reject) => {
+    const workTime = "8 hours";
+    if (workTime === "8 hours") {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+}
+workTime()
+  .then(() => {
+    console.log("In working time now");
+  })
+  .then(() => {
+    console.log("Task 2");
+    console.log("Task 3");
+  })
+  .catch(() => {
+    console.log("Can doing something else with the free time ");
+  });
 
+// 4 using async/await
 async function fetchData() {
   try {
     const response = await fetch("https://api.example.com/data");
@@ -51,5 +92,18 @@ async function fetchData() {
     console.error(error);
   }
 }
-
 fetchData();
+
+async function people() {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1");
+    if (!res.ok) {
+      throw new Error(`something went wrong ${res.status}`);
+    }
+    const person = await res.json();
+    console.log(person.name);
+  } catch (error) {
+    console.log(error);
+  }
+}
+people();
